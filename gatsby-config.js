aa853c288a1faf36ee5d960da59fe652767a9249
Spyros Argalias <sargalias@gmail.com>
2018-12-08 02:47:40 +0000

    Set up project.

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        components: path.join(__dirname, 'src/components'),
+        creations: path.join(__dirname, 'src/creations'),
+      },
+    },
+  ],
+};
