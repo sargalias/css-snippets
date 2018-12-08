b3cc7e8989852ef59b87bf40a3c824abbe889ea3
Spyros Argalias <sargalias@gmail.com>
2018-12-08 03:01:34 +0000

    Add main entry file and header.

---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './header.scss';
+
+export default () => (
+  <header className="header">
+    <h1 className="header__title">100 Days CSS Challenge</h1>
+    <p className="paragraph">My attempt at the  <cite><a href="https://100dayscss.com/" title="100 Days CSS" target="_blank">100 Days CSS</a></cite> challenge.</p>
+  </header>
+);
