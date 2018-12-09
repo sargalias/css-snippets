62fe5e77a0773bc0bf151fd1fd3b0889d7b5f014
Spyros Argalias <sargalias@gmail.com>
2018-12-09 03:21:59 +0000

    Add rolling gear balls thing.

--- src/cssCreations/rollingBall/RollingBall.js
+++ src/cssCreations/rollingBall/RollingBall.js
@@ -5,11 +5,14 @@ import './rollingBall.scss';
 export default () => (
   <CssCreation heading="Rolling Ball">
     <div className="rolling-ball">
-      <div className="ball-rolling-around">
-        <div className="outer-ring">
-          <div className="anchor-ring">
-            <div className="ball" />
-          </div>
+      <div className="outer-ring">
+        <div className="anchor-ring">
+          <div className="center-ring"></div>
+          <div className="ball ball-1" />
+          <div className="ball ball-2" />
+          <div className="ball ball-3" />
+          <div className="ball ball-4" />
+          <div className="center-point"></div>
         </div>
       </div>
     </div>
