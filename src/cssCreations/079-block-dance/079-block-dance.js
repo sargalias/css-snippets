7be4d18df31f342008fd350a0a4639cafc8e88ab
Spyros Argalias <sargalias@gmail.com>
2018-12-15 14:47:17 +0000

    Set up initial code for block-dance.

--- src/cssCreations/079-block-dance/079-block-dance.js
+++ src/cssCreations/079-block-dance/079-block-dance.js
@@ -2,9 +2,17 @@ import React from 'react';
 import CssCreation from 'components/cssCreation/CssCreation';
 import './079-block-dance.scss';
 
+const createBlocks = (numBlocks=10) => {
+  const blocks = [];
+  for (let i=1; i<=numBlocks; i++)
+    blocks.push(<div className={`block block-${i}`} key={i} />);
+  return blocks;
+};
+
 export default () => (
   <CssCreation heading="079 - Block dance">
     <div className="block-dance">
+      { createBlocks(1) }
     </div>
   </CssCreation>
 );
