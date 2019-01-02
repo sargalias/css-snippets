05fa5a66e5c98947cd8e68950a688efe31f0fa04
Spyros Argalias <sargalias@gmail.com>
2019-01-02 04:45:29 +0000

    Fix indentation.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import CssCreationsList from 'components/cssCreationsList/CssCreationsList';
 export default () => (
   <div>
     <Helmet>
-        <meta charSet="utf-8" />
-        <title>100 Days CSS | Spyros Argalias</title>
-        <meta name="description" content="My attempt at 100 Days CSS, creating something different with CSS every day for 100 days." />
-        <html lang="en" />
+      <meta charSet="utf-8" />
+      <title>100 Days CSS | Spyros Argalias</title>
+      <meta name="description" content="My attempt at 100 Days CSS, creating something different with CSS every day for 100 days." />
+      <html lang="en" />
     </Helmet>
 
     <Header />
