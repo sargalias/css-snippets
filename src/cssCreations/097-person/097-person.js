eb9cce6a4e1d744fe61fd4d7cd9f2e03c2080820
Spyros Argalias <sargalias@gmail.com>
2019-01-04 06:29:28 +0000

    097-person: Add CSS variable for person height.

--- src/cssCreations/097-person/097-person.js
+++ src/cssCreations/097-person/097-person.js
@@ -5,19 +5,21 @@ import './097-person.scss';
 export default () => (
   <CssCreation heading="097 - Person" >
     <div className="person">
-      <div className="head" />
-      <div className="body">
-        <div className="arms">
+      <div className="container">
+        <div className="head" />
+        <div className="body">
+          <div className="arms">
             <div className="arm-upper arm-upper-left">
               <div className="arm-lower arm-lower-left" />
             </div>
-          <div className="arm-upper arm-upper-right">
-            <div className="arm-lower arm-lower-right" />
+            <div className="arm-upper arm-upper-right">
+              <div className="arm-lower arm-lower-right" />
+            </div>
           </div>
-        </div>
-        <div className="legs">
-          <div className="leg leg-left"/>
-          <div className="leg leg-right"/>
+          <div className="legs">
+            <div className="leg leg-left"/>
+            <div className="leg leg-right"/>
+          </div>
         </div>
       </div>
     </div>
