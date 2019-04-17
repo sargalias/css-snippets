c7101790e1462f01c988d0219f4e71df765e6b5f
Spyros Argalias <sargalias@gmail.com>
2019-04-17 21:00:10 +0100

    Pause animations when cssCreation is not visible.

--- src/components/cssCreationWrapper/CssCreationWrapper.js
+++ src/components/cssCreationWrapper/CssCreationWrapper.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import IntersectionVisible from 'react-intersection-visible';
 import './cssCreationWrapper.scss';
 
-export default ({ heading, children }) => (
-  <article className="css-creation">
-    <h3 className="css-creation__heading">{ heading }</h3>
-    { children }
-  </article>
-);
+class CssCreationWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.onHide = this.onHide.bind(this);
+    this.onShow = this.onShow.bind(this);
+  }
+
+  onHide(entries) {
+    this.ref.current.style.setProperty('--animation-play-state', 'paused');
+  }
+
+  onShow(entries) {
+    this.ref.current.style.setProperty('--animation-play-state', 'running');
+  }
+
+  render() {
+    return (
+      <IntersectionVisible onHide={this.onHide} onShow={this.onShow}>
+        <article className="css-creation" ref={this.ref}>
+          <h3 className="css-creation__heading">{this.props.heading}</h3>
+          {this.props.children}
+        </article>
+      </IntersectionVisible>
+    );
+  }
+}
+
+export default CssCreationWrapper;
